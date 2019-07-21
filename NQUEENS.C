8fe807a2e9c8a350d605bedd563f72b712a8004a
Nihar Koli <42617538+nihar-koli@users.noreply.github.com>
2019-07-22 02:09:28 +0530

    Added nqueens problem

---
+++ NQUEENS.C
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int n;
+int x[10]={0};
+
+void display()
+{
+   int i,j;
+
+   for(i=1;i<=n;i++)
+   {
+      for(j=1;j<=n;j++)
+      {
+	 if(x[i]==j)
+	    printf("Q ");
+	 else
+	    printf("0 ");
+      }
+      printf("\n");
+   }
+   printf("\n\n");
+}
+
+void nextvalue(int k)
+{
+   int j;
+   label2 :
+   x[k]=(x[k]+1)%(n+1);
+   if(x[k]==0)
+      return;
+
+   for(j=1;j<k;j++)
+      if((x[k]==x[j]) || (abs(x[k]-x[j])==abs(k-j)))
+	  goto label2;
+   return;
+}
+
+void nqueens(int k)
+{
+   label : nextvalue(k);
+
+   if(x[k]==0)
+      return;
+   if(k==n)
+      display();
+   else
+      nqueens(k+1);
+   goto label;
+}
+
+void main()
+{
+   clrscr();
+   printf("\n\n Enter n : ");
+   scanf("%d",&n);
+   nqueens(1);
+   getch();
+}
